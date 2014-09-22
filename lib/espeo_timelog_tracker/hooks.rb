1f60ff5a68b700eb3b3e1e521c64c04e42f6cb52
Jacek Tomaszewski <jacek@jtom.me>
2014-09-22 14:55:58 +0200

    bugfix

--- lib/espeo_timelog_tracker/hooks.rb
+++ lib/espeo_timelog_tracker/hooks.rb
@@ -1,25 +1,43 @@
 module EspeoTimelogTracker
   class Hooks < Redmine::Hook::ViewListener
-
     # Add timelog.js to <head></head> of every page.
     def view_layouts_base_body_bottom(context = {})
       return unless User.current.logged?
       return unless User.current.allowed_to?(:log_time, context[:project], {global: context[:project].nil?})
 
-      # This is unfortunately needed to allow
-      # the use of following methods in the _timelog_tracker.html view:
-      # - #activity_collection_for_select_options
-      unless context[:controller].view_context.respond_to?(:activity_collection_for_select_options)
-        context[:controller].view_context.extend TimelogHelper
-      end
-
       context[:controller].send(:render_to_string, {
         :partial => "timelog_tracker",
         :locals => context.merge({
-          current_tracked_time_entry: TrackedTimeEntry.find_by_user_id(User.current.id)
+          current_tracked_time_entry: TrackedTimeEntry.find_by_user_id(User.current.id),
+          activity_collection_for_timelog_tracker_select_options: activity_collection_for_select_options(nil, @project)
         })
       })
     end
+
+    private
+
+      # Returns a collection of activities for a select field.  time_entry
+      # is optional and will be used to check if the selected TimeEntryActivity
+      # is active.
+      # 
+      # Source: TimelogHelper (app/helpers/timelog_helper.rb)
+      def activity_collection_for_select_options(time_entry=nil, project=nil)
+        project ||= @project
+        if project.nil?
+          activities = TimeEntryActivity.shared.active
+        else
+          activities = project.activities
+        end
+
+        collection = []
+        if time_entry && time_entry.activity && !time_entry.activity.active?
+          collection << [ "--- #{l(:actionview_instancetag_blank_option)} ---", '' ]
+        else
+          collection << [ "--- #{l(:actionview_instancetag_blank_option)} ---", '' ] unless activities.detect(&:is_default)
+        end
+        activities.each { |a| collection << [a.name, a.id] }
+        collection
+      end
 
   end
 end
